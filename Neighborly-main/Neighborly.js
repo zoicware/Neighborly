0d0d9eafe26e4c4dafec1358d6610b482b93c097
i-parker <133996660+i-parker@users.noreply.github.com>
2023-07-18 16:46:19 -0400

    Add files via upload
    First commit

---
+++ Neighborly-main/Neighborly.js
@@ -0,0 +1,30 @@
+// Java Script file --- Neighborly
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+
+    if(nameInput.value=== '' || emailInput.value=== ''){
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
+
+        setTimeout(() => msg.remove(), 3000);
+    } else{
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+
+
+        userList.appendChild(li);
+
+        // Clear fields
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
